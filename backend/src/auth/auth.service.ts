152eea8461a652433247062b8caf0dee6f530aa1
DeLineOfficial <artem.stolyar.2002@mail.ru>
2026-06-15 14:13:47 +0300

    фикс билд

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
